82bed5c675ad8f12296e49400ba99a111b071f11
Paulo <pauloh2012sul@gmail.compwd>
2021-08-13 15:31:17 -0300

    datatable

--- src/app/meus-orcamentos/meus-orcamentos.component.ts
+++ src/app/meus-orcamentos/meus-orcamentos.component.ts
@@ -19,10 +19,12 @@ export class MeusOrcamentosComponent implements OnInit {
   ngOnInit() {
 
     this.listOrcs(this.user.id);
-    
+
   }
 
-  dataTable(){
+  dataTable() {
+
+    //https://datatables.net/reference/option/language
 
     var $ = require('jquery');
 
@@ -30,19 +32,46 @@ export class MeusOrcamentosComponent implements OnInit {
 
     this.changeDetector.detectChanges();
 
-    $('table').DataTable();
+    $('table').DataTable({
+      "language": {
+        "decimal": ",",
+        "emptyTable": "No data available in table",
+        "info": "Exibindo do _START_ ao _END_ de _TOTAL_ registros",
+        "infoEmpty": "Exibindo 0 ao 0 de 0 registros",
+        "infoFiltered": "(filtrado de _MAX_ registros no total)",
+        "infoPostFix": "",
+        "thousands": ".",
+        "lengthMenu": "Exibindo _MENU_ registros",
+        "loadingRecords": "Carregando...",
+        "processing": "Processando...",
+        "search": "Pesquisar:",
+        "zeroRecords": "Nenhum registro encontrado",
+        "paginate": {
+          "first": "Primeiro",
+          "last": "Último",
+          "next": "Próximo",
+          "previous": "Anterior"
+        },
+        "aria": {
+          "sortAscending": ": activate to sort column ascending",
+          "sortDescending": ": activate to sort column descending"
+        }
+      }
+    });
 
 
   }
 
-   //LIST PENDING ORCS
-   listOrcs(idUser: string) {
+  //LIST PENDING ORCS
+  listOrcs(idUser: string) {
 
     this.accountService.listOrcs(idUser)
       .pipe(first())
       .subscribe(x => {
 
         this.orcamentos = x['orcamentos'];
+
+        console.log(this.orcamentos)
 
         this.dataTable();
 
